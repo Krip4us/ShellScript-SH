36752b5737f46c43c1f6ee5c3b92d0dcd8c15987
SPARTAN <44360877+Krip4us@users.noreply.github.com>
2021-09-25 12:06:20 +0200

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -6,35 +6,31 @@
 # este script es solo para root del sistema
 # primero se ejecutara FILE ANTERIOR BACKUP CONFIG
 # regla de copias: margen 30dias
-
 #! /bin/bash 
-	function saludo(){
-		# What to backup. BACKUP FILES 
-            # separate by spaces
-		echo "What to backup" ; read backup_files
-
-		# Where to backup to. DESTINATION
-		echo "Where to backup to" ; read dest
-
-		# Create archive filename. CONFIG ARCHIVE BACKUP
-		day=$(date +"%m%d%Y")
-		hostname=$(hostname -s)
-		archive_file="$hostname-$day.tgz"
-		echo "CONFIG: Backing up $backup_files to $dest/$archive_file"
-		echo "CONFIG: today is $day : backing up only $day files"
-		
-		# FILE ANTERIOR BACKUP CONFIG
-		system_name=$(hostname -s)
-		date_anterior=$(date +"%m%d%Y" -d "-30 days")
-		file_anterior_backup="$system_name-$date_anterior.tgz"
-		# las copias se realizaran con las siguientes condiciones: margen de 1 mes
-		if [ -f $dest/$file_anterior_backup ]; then
-			echo "CONFIG(file_anterior_backup_config): la copia no se puede realizar"
-			exit
-		else
-			# la copia se realiza si todas las condiciones anteriores se incumplen
-			echo "CONFIG: la copia se creara"
-		fi
+function saludo(){
+	# What to backup. BACKUP FILES 
+        # separate by spaces
+	echo "What to backup" ; read backup_files
+	# Where to backup to. DESTINATION
+	echo "Where to backup to" ; read dest
+	# Create archive filename. CONFIG ARCHIVE BACKUP
+	day=$(date +"%m%d%Y")
+	hostname=$(hostname -s)
+	archive_file="$hostname-$day.tgz"
+	echo "CONFIG: Backing up $backup_files to $dest/$archive_file"
+	echo "CONFIG: today is $day : backing up only $day files"	
+	# FILE ANTERIOR BACKUP CONFIG
+	system_name=$(hostname -s)
+	date_anterior=$(date +"%m%d%Y" -d "-30 days")
+	file_anterior_backup="$system_name-$date_anterior.tgz"
+	# las copias se realizaran con las siguientes condiciones: margen de 1 mes
+	if [ -f $dest/$file_anterior_backup ]; then
+		echo "CONFIG(file_anterior_backup_config): la copia no se puede realizar"
+		exit
+	else
+		# la copia se realiza si todas las condiciones anteriores se incumplen
+		echo "CONFIG: la copia se creara"
+	fi
 }
 saludo
 
@@ -52,7 +48,7 @@ function backup(){
 			then
 				sudo tar czf $dest/$archive_file $backup_files
 			elif [ $ask_continue == "n" ]; 
-            then
+            		then
 				echo "session closed by the user"
 				exit
 			else
